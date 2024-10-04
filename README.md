45010a6cca1e60ceabd16c0cebebc521b1f9f6bb
Chris Clarke <1chrisclarke2@gmail.com>
2024-10-03 22:54:55 -0400

    Add initial main page template

---
+++ README.md
@@ -0,0 +1,35 @@
+# Ultimate Lineup Picker
+
+Ultimate Lineup Picker is a tool that uses Sportsbook player props to determine the ideal fantasy football lineup. By analyzing how Vegas believes players will perform, ULP can infer fantasy points scored. When given a team of players, ULP will use these inferences to generate the ideal lineup for the upcoming week of play.
+
+## Table of Contents
+
+- [Development Setup](#development-setup)
+- [Planned Features](#planned-features)
+- [Stretch Goals](#stretch-goals)
+- [Tools Used](#tools-used)
+
+## Development Setup
+
+To set up your development environment for this project, follow these steps:
+
+TODO: Finish development setup
+
+## Planned Features
+
+- [ ] Create a GUI that intakes a fantasy football team of preset size
+- [ ] Create a backend that persists player data
+- [ ] Query a sportsbook API for player props based on player data
+- [ ] Persist results from API query
+- [ ] Create algorithm to translate player props for QB, RB, WR, TE, D/ST, and K into inferred fantasy points
+- [ ] Pick ideal fantasy lineup based on inferred fantasy points
+
+## Stretch Goals
+
+- [ ] Allow users to pick PPR, half-PPR, and Standard scoring
+- [ ] Allow users to adjust point values for player stats (ie, change how many points per yard, value of a TD, etc)
+- [ ] Allow users to adjust team size and player amount (ie, 2 QB, Super Flex, 1 WR, etc)
+
+## Tools Used
+
+TODO: Finish tools used section
---
+++ frontend/src/components/MainPage.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="frontpage">
+    <h1>{{ msg }}</h1>
+    <p>
+      The Ultimate Lineup Picker is a tool that uses sportsbook player props to infer fantasy points for players.
+    </p>
+    <p>
+      Using these inferred points, it will automatically set up the best lineup given a particular team.
+    </p>
+    <h2>Input Team Below</h2>
+    <div class="inputSection">
+      <div class="playerInput" id="qb">
+          <label>Quarterback: </label>
+          <input type="text">
+      </div>
+      <div class="playerInput" id="rb">
+          <label>Running Back 1: </label>
+          <input type="text">
+      </div>
+      <div class="playerInput" id="rb">
+          <label>Running Back 2: </label>
+          <input type="text">
+      </div>
+      <div class="playerInput" id="wr">
+          <label>Wide Receiver 1: </label>
+          <input type="text">
+      </div>
+      <div class="playerInput" id="wr">
+          <label>Wide Receiver 2: </label>
+          <input type="text">
+      </div>
+      <div class="playerInput" id="te">
+          <label>Tight End: </label>
+          <input type="text">
+      </div>
+      <div class="playerInput" id="flex">
+          <label>Flex: </label>
+          <input type="text">
+      </div>
+      <div class="playerInput" id="dst">
+          <label>Defense/Special Teams: </label>
+          <input type="text">
+      </div>
+      <div class="playerInput" id="k">
+          <label>Kicker: </label>
+          <input type="text">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainPage',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.playerInput {
+  padding: 5px;
+  border-radius: 5px;
+  margin-bottom: 5px;
+}
+
+.playerInput>label {
+  display: inline-block;
+  width: 200px;
+}
+</style>
